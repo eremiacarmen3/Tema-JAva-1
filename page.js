fd8a8f862b0aebeabfc073eae9b1a304a3d84c60
eremiacarmen3 <152427986+eremiacarmen3@users.noreply.github.com>
2023-12-11 18:39:38 +0200

    Add files via upload

---
+++ page.js
@@ -0,0 +1,45 @@
+function boolFunction(number){
+    if(number % 10 ===0){
+        console.log("Adevarat")
+    }
+    else{
+        console.log("Fals")
+    }
+}
+
+boolFunction(100)
+function tellFortune(N,Z,Y,X){
+    console.log("Vei fi un "+ X +"in" +Y +"casatorit cu" +Z+"si vei avea"+ N +"copii")
+}
+tellFortune(3,"Emanuel","Italia","Programator")
+
+function dayOfTheWeek(x) {
+ switch(x){
+   case 1:
+    //code block
+    console.log("Monday")
+    break;
+    case 2:
+    //code block
+    console.log("Tuesday")
+    break;
+    case 3:
+    //code block
+    console.log("Wednesday")
+    break;
+    case 4:
+    //code block
+    console.log("Thursday")
+    break;
+    case 5:
+    //code block
+    console.log("Friday")
+    break;
+    case 6:
+    //code block
+    console.log("Saturday")
+    break;
+    //code block
+    console.log("Sunday") 
+}
+dayOfTheWeek(1)
